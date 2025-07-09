29790dcd77f9bfabc720ee84133d73269fe47400
goriponsoft <goriponsoft@gmail.com>
2025-07-09 19:36:46 +0900

    Fixed external PS/2 keymap
    Fixed JP106 and US101 keyboard keymaps for external PS/2 keyboards.

--- esemsx3/src/peripheral/keymap.vhd
+++ esemsx3/src/peripheral/keymap.vhd
@@ -145,8 +145,8 @@ constant rom101 : rom_1024 := (
 -- 101 keyboard (set 2) / Shift = OFF
 --      PS/2 Scan Code XX
         X"7F", X"3F", X"7F", X"17", X"76", X"56", X"66", X"0F", -- 00
-        X"7F", X"2F", X"67", X"26", X"07", X"37", X"B1", X"7F", -- 08
-        X"7F", X"26", X"86", X"46", X"16", X"64", X"10", X"7F", -- 10
+        X"7F", X"2F", X"67", X"26", X"07", X"37", X"D1", X"7F", -- 08
+        X"7F", X"26", X"86", X"FF", X"16", X"64", X"10", X"7F", -- 10
         X"7F", X"7F", X"75", X"05", X"62", X"45", X"20", X"7F", -- 18
         X"7F", X"03", X"55", X"13", X"23", X"40", X"30", X"7F", -- 20
         X"7F", X"08", X"35", X"33", X"15", X"74", X"50", X"7F", -- 28
@@ -154,10 +154,10 @@ constant rom101 : rom_1024 := (
         X"7F", X"7F", X"24", X"73", X"25", X"70", X"01", X"7F", -- 38
         X"7F", X"22", X"04", X"63", X"44", X"00", X"11", X"7F", -- 40
         X"7F", X"32", X"42", X"14", X"71", X"54", X"21", X"7F", -- 48
-        X"7F", X"52", X"A0", X"7F", X"61", X"A1", X"7F", X"7F", -- 50
+        X"7F", X"7F", X"F0", X"7F", X"61", X"A1", X"7F", X"7F", -- 50
         X"36", X"86", X"77", X"12", X"7F", X"41", X"7F", X"7F", -- 58
-        X"7F", X"41", X"7F", X"7F", X"1B", X"7F", X"57", X"3B", -- 60
-        X"7F", X"49", X"41", X"79", X"2A", X"C2", X"7F", X"7F", -- 68
+        X"7F", X"7F", X"7F", X"7F", X"7F", X"7F", X"57", X"7F", -- 60
+        X"7F", X"49", X"7F", X"79", X"2A", X"7F", X"7F", X"7F", -- 68
         X"39", X"7A", X"59", X"0A", X"1A", X"3A", X"27", X"6A", -- 70
         X"1F", X"19", X"69", X"5A", X"09", X"4A", X"7E", X"7F", -- 78
         X"7F", X"7F", X"7F", X"46", X"7F", X"7F", X"7F", X"7F", -- 80
@@ -180,7 +180,7 @@ constant rom101 : rom_1024 := (
 --      PS/2 Scan Code E0 XX
         X"7F", X"7F", X"7F", X"7F", X"7F", X"7F", X"7F", X"7F", -- 00
         X"7F", X"7F", X"7F", X"7F", X"7F", X"7F", X"7F", X"7F", -- 08
-        X"7F", X"3B", X"7F", X"7F", X"1B", X"7F", X"7F", X"7F", -- 10
+        X"7F", X"26", X"7F", X"7F", X"16", X"7F", X"7F", X"7F", -- 10
         X"7F", X"7F", X"7F", X"7F", X"7F", X"7F", X"7F", X"08", -- 18  (LWIN = 0xE0, 0x1F => SPACE)
         X"7F", X"7F", X"7F", X"7F", X"7F", X"7F", X"7F", X"08", -- 20  (RWIN = 0xE0, 0x27 => SPACE)
         X"7F", X"7F", X"7F", X"7F", X"7F", X"7F", X"7F", X"46", -- 28  (APP  = 0xE0, 0x2F => KANA )
@@ -214,8 +214,8 @@ constant rom101 : rom_1024 := (
 -- 101 keyboard (set 2) / Shift = ON
 --      PS/2 Scan code XX with Shift
         X"FF", X"BF", X"FF", X"97", X"F6", X"D6", X"E6", X"8F", -- 00
-        X"FF", X"AF", X"E7", X"A6", X"87", X"B7", X"D1", X"FF", -- 08
-        X"FF", X"A6", X"86", X"C6", X"96", X"E4", X"90", X"FF", -- 10
+        X"FF", X"AF", X"E7", X"A6", X"87", X"B7", X"B1", X"FF", -- 08
+        X"FF", X"A6", X"86", X"FF", X"96", X"E4", X"90", X"FF", -- 10
         X"FF", X"FF", X"F5", X"85", X"E2", X"C5", X"51", X"FF", -- 18
         X"FF", X"83", X"D5", X"93", X"A3", X"C0", X"B0", X"FF", -- 20
         X"FF", X"88", X"B5", X"B3", X"95", X"F4", X"D0", X"FF", -- 28
@@ -223,10 +223,10 @@ constant rom101 : rom_1024 := (
         X"FF", X"FF", X"A4", X"F3", X"A5", X"E0", X"82", X"FF", -- 38
         X"FF", X"A2", X"84", X"E3", X"C4", X"91", X"81", X"FF", -- 40
         X"FF", X"B2", X"C2", X"94", X"02", X"D4", X"D2", X"FF", -- 48
-        X"FF", X"D2", X"F0", X"FF", X"E1", X"F1", X"FF", X"FF", -- 50
+        X"FF", X"FF", X"A0", X"FF", X"E1", X"F1", X"FF", X"FF", -- 50
         X"B6", X"86", X"F7", X"92", X"FF", X"C1", X"FF", X"FF", -- 58
-        X"FF", X"C1", X"FF", X"FF", X"9B", X"FF", X"D7", X"BB", -- 60
-        X"FF", X"C9", X"C1", X"F9", X"AA", X"C2", X"FF", X"FF", -- 68
+        X"FF", X"FF", X"FF", X"FF", X"FF", X"FF", X"D7", X"FF", -- 60
+        X"FF", X"C9", X"FF", X"F9", X"AA", X"FF", X"FF", X"FF", -- 68
         X"B9", X"FA", X"D9", X"8A", X"9A", X"BA", X"A7", X"EA", -- 70
         X"9F", X"99", X"E9", X"DA", X"89", X"CA", X"FE", X"FF", -- 78
         X"FF", X"FF", X"FF", X"C6", X"FF", X"FF", X"FF", X"FF", -- 80
@@ -249,7 +249,7 @@ constant rom101 : rom_1024 := (
 --      PS/2 Scan code E0 XX with Shift
         X"FF", X"FF", X"FF", X"FF", X"FF", X"FF", X"FF", X"FF", -- 00
         X"FF", X"FF", X"FF", X"FF", X"FF", X"FF", X"FF", X"FF", -- 08
-        X"FF", X"BB", X"FF", X"FF", X"9B", X"FF", X"FF", X"FF", -- 10
+        X"FF", X"A6", X"FF", X"FF", X"96", X"FF", X"FF", X"FF", -- 10
         X"FF", X"FF", X"FF", X"FF", X"FF", X"FF", X"FF", X"88", -- 18  (LWIN = 0xE0, 0x1F => SHIFT + SPACE)
         X"FF", X"FF", X"FF", X"FF", X"FF", X"FF", X"FF", X"88", -- 20  (RWIN = 0xE0, 0x27 => SHIFT + SPACE)
         X"FF", X"FF", X"FF", X"FF", X"FF", X"FF", X"FF", X"C6", -- 28  (APP  = 0xE0, 0x2F => SHIFT + KANA )
@@ -299,7 +299,7 @@ constant rom106 : rom_512 := (
 -- Keymap for 106 keyboard (set 2)
 --      PS/2 Scan Code XX
         X"FF", X"3F", X"FF", X"17", X"76", X"56", X"66", X"0F", -- 00
-        X"FF", X"2F", X"67", X"26", X"07", X"37", X"52", X"FF", -- 08
+        X"FF", X"2F", X"67", X"26", X"07", X"37", X"27", X"FF", -- 08
         X"FF", X"26", X"06", X"46", X"16", X"64", X"10", X"FF", -- 10
         X"FF", X"FF", X"75", X"05", X"62", X"45", X"20", X"FF", -- 18
         X"FF", X"03", X"55", X"13", X"23", X"40", X"30", X"FF", -- 20
@@ -308,9 +308,9 @@ constant rom106 : rom_512 := (
         X"FF", X"FF", X"24", X"73", X"25", X"70", X"01", X"FF", -- 38
         X"FF", X"22", X"04", X"63", X"44", X"00", X"11", X"FF", -- 40
         X"FF", X"32", X"42", X"14", X"71", X"54", X"21", X"FF", -- 48
-        X"FF", X"52", X"02", X"FF", X"51", X"31", X"FF", X"FF", -- 50
+        X"FF", X"41", X"02", X"FF", X"51", X"31", X"FF", X"FF", -- 50
         X"36", X"06", X"77", X"61", X"FF", X"12", X"FF", X"FF", -- 58
-        X"FF", X"41", X"FF", X"FF", X"1B", X"FF", X"57", X"3B", -- 60
+        X"FF", X"FF", X"FF", X"FF", X"1B", X"FF", X"57", X"3B", -- 60
         X"FF", X"49", X"41", X"79", X"2A", X"FF", X"FF", X"FF", -- 68
         X"39", X"7A", X"59", X"0A", X"1A", X"3A", X"27", X"6A", -- 70
         X"1F", X"19", X"69", X"5A", X"09", X"4A", X"7E", X"FF", -- 78
@@ -374,10 +374,10 @@ constant rom_int_cnv : rom_1024 := (
 -- F6     ($0B)     : [GRAPH]    ($26)
 -- F7     ($83)     : [KANA]     ($46) ** KANA Default
 -- F8     ($0A)     : [SELECT]   ($67)
--- HOME   ($E0 $6C) : [HOME/CLS] ($18) ** FN+Left
--- END    ($E0 $69) : [STOP]     ($47) ** FN+Right
--- ALT L  ($11)     : [GRAPH]    ($26) ** FN2 default
+-- Home   ($E0 $6C) : [HOME/CLS] ($18) ** FN+Left
+-- End    ($E0 $69) : [STOP]     ($47) ** FN+Right
 -- Insert ($E0 $70) : [INS]      ($28) ** INS default
+-- Alt(L) ($11)     : [GRAPH]    ($26) ** FN2 default
 --
 -- PS/2 KEYS        : OCM-PLD KEYS
 -------------------------------------
@@ -532,17 +532,15 @@ constant rom_int_cnv : rom_1024 := (
 constant rom_int_ntv : rom_512 := (
 
 -- Special keys for Built-in Native keyboard
------------------------------------------
--- Special keys for Built-in Conversion Keyboard
 -- PS/2 KEYS        : MSX KEYS
 -------------------------------------
 -- F6     ($0B)     : [GRAPH]    ($26)
 -- F7     ($83)     : [KANA]     ($46) ** KANA Default
 -- F8     ($0A)     : [SELECT]   ($67)
--- HOME   ($E0 $6C) : [HOME/CLS] ($18) ** FN+Left
--- END    ($E0 $69) : [STOP]     ($47) ** FN+Right
+-- Home   ($E0 $6C) : [HOME/CLS] ($18) ** FN+Left
+-- End    ($E0 $69) : [STOP]     ($47) ** FN+Right
 -- Insert ($E0 $70) : [INS]      ($28) ** INS default
--- ALT L  ($11)     : [_]        ($52) ** FN2 default
+-- Alt(L) ($11)     : [_]        ($52) ** FN2 default
 --
 -- PS/2 KEYS        : OCM-PLD KEYS
 -------------------------------------
